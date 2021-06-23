bcd2645c3dfdd32f74240f4645a8909d18397276
IlyaKurskoj <86369293+IlyaKurskoj@users.noreply.github.com>
2021-06-23 23:47:41 +0300

    Initial commit

---
+++ Lists.py
@@ -0,0 +1,10 @@
+name="Meat"
+annotation="give to u flesh leeps"
+time_of_cooking=7
+disc="fresh meat"
+step=3
+ingr_list='meat'
+recipe_list=[name,annotation,time_of_cooking,disc,step,ingr_list]
+print(recipe_list)
+list1=recipe_list[:3]
+print(list1)
